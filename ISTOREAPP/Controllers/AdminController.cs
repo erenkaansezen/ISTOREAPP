1343cb84a0e9987757bdc500ab718b97d80756f2
eren sezen <erenkaansez17@gmail.com>
2024-12-18 02:58:50 +0300

    Proje dosyası ekle.

---
+++ ISTOREAPP/Controllers/AdminController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ISTOREAPP.Controllers
+{
+    public class AdminController:Controller
+    {
+        private UserManager<IdentityUser> _userManager;
+        public AdminController(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        public IActionResult UsersList()
+        {
+            return View(_userManager.Users);
+        }
+        public IActionResult UserCreate()
+        {
+            return View();
+        }
+    }
+}
---
+++ ISTOREAPP/Models/IdentitySeedData.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace ISTOREAPP.Models
+{
+    public class IdentitySeedData
+    {
+        private const string adminUser = "admin";
+        private const string adminPassword = "Admin_123";
+
+        public static async void IdentityTestUser(IApplicationBuilder app)
+        {
+            var context = app.ApplicationServices.CreateScope().ServiceProvider.GetRequiredService<IdentityContext>();
+            if (context.Database.GetAppliedMigrations().Any()) 
+            {
+                context.Database.Migrate();
+            }
+            
+            var userManager = app.ApplicationServices.CreateScope().ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
+
+            var user = await userManager.FindByNameAsync(adminUser);
+
+            if (user == null)
+            {
+                user = new IdentityUser
+                {
+                    UserName = adminUser,
+                    Email = "admin@ıstore.com",
+                    PhoneNumber = "1234567890",
+
+
+                };
+                await userManager.CreateAsync(user, adminPassword);
+            }
+        }
+    }
+}
