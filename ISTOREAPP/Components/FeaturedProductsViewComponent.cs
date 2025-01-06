7f0ee250a66b551c6e826e944d63d7e541a12b1e
erensezen <erenkaansez17@gmail.com>
2025-01-06 14:03:19 +0300

    ViewCompanentDevolopment

---
+++ ISTOREAPP/Components/FeaturedProductsViewComponent.cs
@@ -0,0 +1,31 @@
+﻿using Business.Services;
+using ISTOREAPP.Data.Entities;
+using ISTOREAPP.Helpers;
+using ISTOREAPP.Models;
+using ISTOREAPP.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StoreApp.Web.Components;
+
+public class FeaturedProductsViewComponent : ViewComponent
+{
+    private readonly ProductService _productService;
+
+    public FeaturedProductsViewComponent(ProductService productService)
+    {
+        _productService = productService;
+    }
+
+    public async Task <IViewComponentResult> InvokeAsync()
+    {
+        var products = await _productService.GetAllProductsAsync();
+
+        var model = new HomeViewModel
+        {
+
+            Products = products
+        };
+
+        return View(model);
+    }
+}
